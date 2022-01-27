e219060c54d93f7ea27f78459190bf38f539cf52
Luis Garcia <lalejandro.1988@gmail.com>
2022-01-26 20:22:03 -0600

    Initial Commit

---
+++ JsonAppSwiftUI/JsonAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonAppSwiftUIApp.swift
+//  JsonAppSwiftUI
+//
+//  Created by Luis Garcia on 26/01/22.
+//
+
+import SwiftUI
+
+@main
+struct JsonAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
